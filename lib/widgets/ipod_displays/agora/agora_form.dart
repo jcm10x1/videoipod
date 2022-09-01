d77340f63785af1a290464dd41856c8d61496e10
Jacob Miller <68158267+jcm10x1@users.noreply.github.com>
2022-09-01 17:35:44 -0400

    Fixed a bug that moved the cursor to the front when editing the value for the call code using the keyboard.

--- lib/widgets/ipod_displays/agora/agora_form.dart
+++ lib/widgets/ipod_displays/agora/agora_form.dart
@@ -2,23 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:videoipod/models/agora_engine.dart';
-import 'package:videoipod/widgets/display_root.dart';
 
 import '../../display.dart';
 
 class AgoraFormDisplay extends ConsumerWidget {
   AgoraFormDisplay({Key? key}) : super(key: key);
 
+  final controller = TextEditingController();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    ref.watch(agoraEngineProvider);
     final engineNotifier = ref.read(agoraEngineProvider.notifier);
-    final controller = TextEditingController();
+    ref.listen(agoraEngineProvider, (dynamic previous, dynamic next) {
+      final newId = ref.read(agoraEngineProvider.notifier).channelId;
+      if (newId != controller.text) {
+        controller.text = newId!;
+      }
+    });
     controller.text = engineNotifier.channelId ?? "";
     return Display(
       child: Column(
         children: [
-          Text("Enter the call code below or tap generate"),
+          const Text("Enter the call code below or tap generate"),
           PlatformTextField(
             controller: controller,
             onChanged: (value) {
